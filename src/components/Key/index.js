ff47a7e740528a1a775700e9d73c40dfe15ad427
Andreas Rasmussen <andreasrefras@gmail.com>
2018-03-28 14:27:41 +0200

    Finished keyboard layout with the odd enter and arrow up/down buttons and active state size of space button

--- src/components/Key/index.js
+++ src/components/Key/index.js
@@ -4,20 +4,21 @@ import PropTypes from 'prop-types'
 
 export default class Key extends Component {
   render() {
-    const { width, height } = this.props
+    const { className, width, height } = this.props
     const { ...paths } = this.props.paths
 
     const defaultWidth = 52.917
     const defaultHeight = 51.917
 
     return(
-      <KeyWithStyle width={ width ? width : defaultWidth } height={ height ? height : defaultHeight }>
-        <div className="shadowAbove"></div>
+      <KeyWithStyle className={ className } width={ width ? width : defaultWidth } height={ height ? height : defaultHeight }>
+        <div className="shadowAboveActive"></div>
+        <div className="holeBelowExtension"></div>
         <svg className="key" xmlns="http://www.w3.org/2000/svg"
           width={ width ? width : defaultWidth }
           height={ height ? height : 51.917 }
           viewBox={ width || height ? `0 0 ${width ? width : defaultWidth} ${height ? height : defaultHeight}` : '0 0 52.917 51.917' }>
-          <rect className="shadow" x="0" y="0" width={ width ? width : defaultWidth } height={ height ? height : defaultHeight } rx="4" ry="4" />
+          <rect className="hole" x="0" y="0" width={ width ? width : defaultWidth } height={ height ? height : defaultHeight } rx="4" ry="4" />
           <g className={ `plasticAndPrint ${ width ? 'rectangular' : '' }` }>
             <rect className="plastic" x="1" y="1" width={ width ? width - 2 : defaultWidth - 2 } height={ height ? height - 2 : defaultHeight - 2 } rx="3" ry="3" />
             <path className="print" d={ paths.print } />
@@ -29,6 +30,7 @@ export default class Key extends Component {
 }
 
 Key.propTypes = {
+  className: PropTypes.string,
   paths: PropTypes.object,
   width: PropTypes.number,
   height: PropTypes.number
@@ -38,39 +40,97 @@ Key.propTypes = {
 // Packages
 import styled from 'styled-components'
 
+const keyColor = '#f4f3f4';
+const holeColor = '#444';
+const borderRadius = '4px';
+
 const KeyWithStyle = styled.div`
   display: inline-block;
   position: relative;
+  float: left;
   cursor: pointer;
   margin-left: 8px;
   margin-top: 8px;
 
-  &:active {
-    .plasticAndPrint {
-      transform: scale3d(0.985, 0.985, 1);
-      &.rectangular { transform: scale3d(0.991, 0.985, 1) !important; }
+  &.enter {
+    &:after {
+      content: '';
+      position: absolute;
+      top: calc( 100% - 5px );
+      right: 1px;
+      width: 35.993px;
+      height: 64.708px;
+      background-color: ${keyColor};
+      border-bottom-right-radius: 3px;
+      border-bottom-left-radius: 3px;
+      z-index: 3;
     }
-    .shadowAbove { box-shadow: inset 0px 1px 2px rgba(0,0,0,0.25); }
+    .holeBelowExtension {
+      position: absolute;
+      top: calc( 100% - 4px );
+      right: 0;
+      width: 37.993px;
+      height: 64.708px;
+      background-color: ${holeColor};
+      border-bottom-right-radius: 4px;
+      border-bottom-left-radius: 4px;
+    }
   }
 
-  .shadowAbove {
+  &.arrow {
+    &:after {
+      content: '';
+      position: absolute;
+      left: 1px;
+      width: calc(100% - 2px);
+      height: 6px;
+      z-index: 3;
+      transition: background 50ms ease-out;
+    }
+    &.up:after {
+      background: linear-gradient( ${keyColor}, rgba(0,0,0,0.35) ), white;
+      bottom: 0px;
+    }
+    &.down:after {
+      background: linear-gradient( rgba(0,0,0,0.35), ${keyColor} ), white;
+      top: 1px;
+    }
+  }
+
+  .shadowAboveActive {
     position: absolute;
     width: ${ props => props.width - 2 }px;
-    height: ${ props => props.height - 2 }px;
+    height: 4px;
     top: 1px;
     left: 1px;
     border-radius: 3px;
-    transition: box-shadow 50ms ease-out;
-    background: radial-gradient( rgba(0,0,0,0.02) 60%, rgba(0,0,0,0) );
-    box-shadow: inset 0px 0px 4px rgba(255,255,255,1);
+    transition: background 50ms ease-out;
+    background: transparent;
+    z-index: 2;
+  }
+
+  &:active {
+    &.enter:after { transform: scale3d(0.985, 0.985, 1) translateY(-1px); }
+    &.arrow {
+      &:after { transform: scale3d(0.985, 0.985, 1); width: calc(100% - 3px); }
+      &.up:after { background: linear-gradient( ${keyColor}, rgba(0,0,0,0.35) 75%, ${holeColor} ), white; }
+      &.down:after { background: linear-gradient( ${holeColor}, rgba(0,0,0,0.35) 25%, ${keyColor} ), white; }
+    }
+    .plasticAndPrint { transform: scale3d(0.985, 0.985, 0);
+      &.rectangular { transform: scale3d(0.991, 0.985, 1) !important; }
+    }
+    &.space .plasticAndPrint { transform: scale3d(0.996, 0.985, 0) !important; }
+    .shadowAboveActive { background: linear-gradient( rgba(0,0,0,0.5), rgba(0,0,0,0.1) 50%, transparent ); }
   }
 
   .key {
-    .shadow { fill: #444; }
+    position: relative;
+    z-index: 1;
+    .hole { fill: ${holeColor}; }
     .plasticAndPrint {
       transition: transform 50ms linear;
       transform-origin: center center;
-      .plastic { fill: #f4f3f4; }
+      .plastic { fill: ${keyColor}; }
       .print { fill: #666; }
     }
   }
--- src/components/Keyboard/index.js
+++ src/components/Keyboard/index.js
@@ -642,11 +642,25 @@ export default class Keyboard extends Component {
     ]
   }
   render() {
+    const { keys } = this
     return(
       <KeyboardWithStyle>
-        { this.keys.map( ( key, index ) => {
+        { keys.slice(0, 41).map( ( key, index ) => {
           return ( <Key key={ index } paths={ key.paths } width={ key.width } height={ key.height } /> )
         })}
+        <div className="keyWrap">
+          <Key className="enter" paths={ keys[41].paths } width={ keys[41].width } height={ keys[41].height } />
+        </div>
+        { keys.slice(42, 76).map( ( key, index ) => {
+          return ( <Key key={ index } className={ index === 30 ? 'space' : '' } paths={ key.paths } width={ key.width } height={ key.height } /> )
+        })}
+        <div className="keyWrap keysUpDownWrap">
+          <div className="keysUpDown">
+            <Key className="arrow up" paths={ keys[76].paths } width={ keys[76].width } height={ keys[76].height } />
+            <Key className="arrow down" paths={ keys[77].paths } width={ keys[77].width } height={ keys[77].height } />
+          </div>
+        </div>
+        <Key paths={ keys[78].paths } width={ keys[78].width } height={ keys[78].height } />
       </KeyboardWithStyle>
     )
   }
@@ -662,4 +676,31 @@ const KeyboardWithStyle = styled.div`
   width: 892.469px;
   border-radius: 12px;
   box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.4), 5px 5px 10px 0px rgba(0,0,0,0.4);
+  overflow: auto;
+
+
+  .keyWrap { display: inline-block; }
+  .keysUpDownWrap {
+    float: left;
+    .keysUpDown {
+      display: flex;
+      flex-direction: column;
+      padding-top: 8px;
+      padding-left: 8px;
+      > div {
+        margin: 0px;
+        &:first-child {
+          &:before {
+            content: '';
+            position: absolute;
+            top: calc( 100% - 4px );
+            right: 0;
+            width: 100%;
+            height: 8px;
+            background-color: #444;
+          }
+        }
+      }
+    }
+  }
 `
